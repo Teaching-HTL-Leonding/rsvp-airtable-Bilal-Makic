038c9ae8da898fede29c820739dd25040f5b0a5d
Bilal Makic <b.makic@HTBLALeonding.onmicrosoft.com>
2022-12-09 18:23:47 +0100

    WIP

--- RSPVAirtable/src/app/rspv/rspv.service.ts
+++ RSPVAirtable/src/app/rspv/rspv.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 export interface Root {
   records: Record[]
@@ -22,16 +23,12 @@ export interface Fields {
 @Injectable({
   providedIn: 'root'
 })
-export class RspvService implements OnInit{
+
+export class RspvService {
 
   public dataFromServer?: Root;
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-    this.http.get<Root>("https://api.airtable.com/v0/appPBBDWbnh6dxtyc/Guests",
-    {
-      headers: new HttpHeaders({Authorization: "Bearer pataileXpEmRT174E.32b4c99cb46c128b7c3954d4ad1d506b630253b8ada5b067e7b103185d8daea8"})
-    }).subscribe(data => this.dataFromServer = data)
-  }
+
 }
